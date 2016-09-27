c2bcf4101931aa1d69fc6486a2797ec923962e64
Ding, Minyi <dingminyi@foxmail.com>
2016-09-27 15:17:30 +0000

    New token for datayes.com

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     securityID = "002230.XSHE"
     now = datetime.datetime.now()
     stamp = now.strftime("%Y-%m-%d-%H-%M-%S")
-    token = "96a75c3c43df640b4ba477813e58e517dd0df704c608c2060dc984928d8a64ef"
+    token = "6dd2eb167cef9b1622787ea404ced6906bacb288edc55d68408a17a87cebcaf9"
     client = DataFetcher.Client()
     client.init(token)
     code, res = client.getData("/api/market/getTickRTIntraDay.json?field=&startTime=&securityID=" + securityID + "&endTime=")
